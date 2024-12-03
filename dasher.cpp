9d73507559b53639df66f06bbf7653aedb3d8d6a
crubz11 <max@kaiser.family>
2024-12-03 17:00:15 +0100

    added second enemy (will implement better solution later)

--- dasher.cpp
+++ dasher.cpp
@@ -28,8 +28,20 @@ int main()
     Rectangle nebRec{0.f, 0.f, nebula.width / 8, nebula.height / 8};
     Vector2 nebPos{windowWidth, windowHeight - nebRec.height};
 
+    Rectangle neb2Rec{0.f, 0.f, nebula.width / 8, nebula.height / 8};
+    Vector2 neb2Pos{windowWidth + 300, windowHeight - nebRec.height};
+
+    // nebula animation variables
+    int nebFrame = 0;
+    const float nebUpdateTime = 1.f / 12.f;
+    float nebRunningTime = 0;
+
+    int neb2Frame = 0;
+    const float neb2UpdateTime = 1.f / 16.f;
+    float neb2RunningTime = 0;
+
     // nebula x velocity (p/s)
-    int nebVel = -600;
+    int nebVel = -200;
 
     // animation frame
     int frame = 0;
@@ -47,7 +59,7 @@ int main()
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        const float dt = GetFrameTime();
+        const float dT = GetFrameTime();
 
         BeginDrawing();
         ClearBackground(WHITE);
@@ -58,22 +70,11 @@ int main()
             // rect is on the ground
             velocity = 0;
             isInAir = false;
-            if (runningTime >= updateTime)
-            {
-                runningTime = 0;
-                // update animation frame
-                scarfyRec.x = frame * scarfyRec.width;
-                frame++;
-                if (frame > 5)
-                {
-                    frame = 0;
-                }
-            }
         }
         else
         {
             //  apply gravity
-            velocity += gravity * dt;
+            velocity += gravity * dT;
             isInAir = true;
         }
 
@@ -84,15 +85,59 @@ int main()
         }
 
         // update nebula position
-        nebPos.x += nebVel * dt;
+        nebPos.x += nebVel * dT;
+        neb2Pos.x += nebVel * dT;
 
         // update scarfy position
-        scarfyPos.y += velocity * dt;
+        scarfyPos.y += velocity * dT;
 
-        runningTime += dt;
+        // update scarfy animation frame
+        if (!isInAir)
+        {
+            runningTime += dT;
+            if (runningTime >= updateTime)
+            {
+                runningTime = 0.0;
+                // update animation frame
+                scarfyRec.x = frame * scarfyRec.width;
+                frame++;
+                if (frame > 5)
+                {
+                    frame = 0;
+                }
+            }
+        }
+
+        // update nebula animation frame
+        nebRunningTime += dT;
+        if (nebRunningTime >= nebUpdateTime)
+        {
+            nebRunningTime = 0.0;
+            // update animation frame
+            nebRec.x = nebFrame * nebRec.width;
+            nebFrame++;
+            if (nebFrame > 7)
+            {
+                nebFrame = 0;
+            }
+        }
+
+        neb2RunningTime += dT;
+        if (neb2RunningTime >= neb2UpdateTime)
+        {
+            neb2RunningTime = 0.0;
+            // update animation frame
+            neb2Rec.x = neb2Frame * neb2Rec.width;
+            neb2Frame++;
+            if (neb2Frame > 7)
+            {
+                neb2Frame = 0;
+            }
+        }
 
         // Draw Nebula
         DrawTextureRec(nebula, nebRec, nebPos, WHITE);
+        DrawTextureRec(nebula, neb2Rec, neb2Pos, RED);
 
         // Draw Scarfy
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
